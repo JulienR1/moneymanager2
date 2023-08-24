ced381da7c3e3b29c106cd5f2108a3b4cae91530
Julien Rousseau <julrousseau20@gmail.com>
2023-08-24 15:39:42 -0400

    initial routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import "./index.css";
 
-import type { Component } from "solid-js";
+import { Route, Routes } from "@solidjs/router";
+import { Show, type Component } from "solid-js";
+
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App: Component = () => {
-  return <div>hello world</div>;
+  const isLoggedIn = true;
+
+  return (
+    <Routes>
+      <Route path="/">
+        <Show
+          when={isLoggedIn}
+          fallback={<Route path="/" component={Home}></Route>}
+        >
+          <Route path="/" component={Dashboard}></Route>
+        </Show>
+      </Route>
+      <Route path={["register", "login"]} component={Auth} />
+      <Route path="*" component={NotFound} />
+    </Routes>
+  );
 };
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,15 @@
 /* @refresh reload */
+import { Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 
 import App from "./App";
 
 const root = document.getElementById("root");
-render(() => <App />, root!);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  root!,
+);
